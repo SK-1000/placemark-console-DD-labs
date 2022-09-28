e28d6039e5b50de8811684120c7b3bd02972664d
sk1001 <sheilafleming81@gmail.com>
2022-09-28 16:47:09 +0100

    testing

---
+++ src/main/kotlin/org/setu/placemark/console/models/PlacemarkStore.kt
@@ -0,0 +1,8 @@
+import org.setu.placemark.console.models.PlacemarkModel
+
+interface PlacemarkStore {
+    fun findAll(): List<PlacemarkModel>
+    fun findOne(id: Long): PlacemarkModel?
+    fun create(placemark: PlacemarkModel)
+    fun update(placemark: PlacemarkModel)
+}
